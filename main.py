36f96f3c945fd290ae11266728cb1bb8868a1637
Hemang Murugan <52290764+iamtheretronerd@users.noreply.github.com>
2021-05-27 19:19:55 +0530

    case2

--- main.py
+++ main.py
@@ -3,12 +3,38 @@ def server(duration,cpu,budget):
   usEastPrice = [2.82,1.4,0.774,0.45,0.23,0.12]
   usWestPrice = [2.97,1.3,0.89,0.413,0.14]
   duration = float(duration)
+  budget = float(budget)
   print(duration,cpu,budget)
   if(cpu == 0):
-    print("Case 2")
+    # print("Case 2")
+    # CASE 2
+    budgetph = float(budget/duration)
+    newlist = [0,0,0,0,0,0]
+    newvar = budgetph
+    # priceperhour = [0,0,0,0,0,0]
+    # totalprice = 0
+
+    for i in range(6):
+        newlist[i] = int(newvar/usEastPrice[i])
+        if(newlist[i] > 0):
+            newvar -= (usEastPrice[i]*newlist[i])
+    #print(newlist)
+
+    print("region US East")
+    print(f"Servers:\nlarge: {newlist[5]}\nxlarge: {newlist[4]}\n2xlarge: {newlist[3]}\n4xlarge: {newlist[2]}\n8xlarge: {newlist[1]}\n10xlarge: {newlist[0]}\n")
+
+    wnewlist = [0,0,0,0,0]
+    wnewvar = budgetph
+    for i in range(5):
+        wnewlist[i] = int(wnewvar/usWestPrice[i])
+        if(wnewlist[i] > 0):
+            wnewvar -= (usWestPrice[i] * wnewlist[i])
+    #print(wnewlist)
+    print("region US West")
+    print(f"Servers:\nlarge: {wnewlist[4]}\n2xlarge: {wnewlist[3]}\n4xlarge: {wnewlist[2]}\n8xlarge: {wnewlist[1]}\n10xlarge: {wnewlist[0]}")
+
   elif(cpu > 0):
     if(budget == 0):
-      if(cpu > 32):
         #CASE 1
         # US EAST
         newlist = [0,0,0,0,0,0]
@@ -32,8 +58,8 @@ def server(duration,cpu,budget):
         pricewduration = finalPrice * duration
 
         print("region US East")
-        print(f"Total Cost {pricewduration}")
-        print(f"Servers:\nlarge: {newprice[5]}\nxlarge: {newprice[4]}\n 2xlarge: {newprice[3]}\n4xlarge: {newprice[2]}\n8xlarge: {newprice[1]}\n10xlarge: {newprice[0]}")
+        print(f"Total Cost ${round(pricewduration,1)}")
+        print(f"Servers:\nlarge: {newlist[5]}\nxlarge: {newlist[4]}\n2xlarge: {newlist[3]}\n4xlarge: {newlist[2]}\n8xlarge: {newlist[1]}\n10xlarge: {newlist[0]}\n")
 
         #US WEST
         
@@ -56,14 +82,16 @@ def server(duration,cpu,budget):
             
         for i in range (5):
             wnewprice[i] = wnewlist[i] * usWestPrice[i]
+            #price of all servers
             
         for i in range (5):
             wfinalPrice = wfinalPrice + wnewprice[i]
         wpricewduration = wfinalPrice * duration
 
         print("region US West")
-        print(f"Total Cost {wpricewduration}")
-        print(f"Servers:\nlarge: {wnewprice[4]}\n 2xlarge: {wnewprice[3]}\n4xlarge: {wnewprice[2]}\n8xlarge: {wnewprice[1]}\n10xlarge: {wnewprice[0]}")
+        print(f"Total Cost ${round(wpricewduration,1)}")
+        print(f"Servers:\nlarge: {wnewlist[4]}\n2xlarge: {wnewlist[3]}\n4xlarge: {wnewlist[2]}\n8xlarge: {wnewlist[1]}\n10xlarge: {wnewlist[0]}")
+      
 
 
       
@@ -74,15 +102,15 @@ def server(duration,cpu,budget):
 if __name__=="__main__":
       name = input("Enter your Name: ")
       print("Hello "+name)
-      duration = input("Enter the duration you want for your servers: ")
+      duration = input("Enter the duration(hours) you want for your servers: ")
      # region = int(input("Enter 1 for US-EAST and 2 for US-WEST: "))
       cpu = int(input("Enter number of CPU, Enter 0 if you dont know: "))
       if (cpu == 0):
-          budget = input("Enter the amount of money you would like to pay: ")
+          budget = input(f"Enter the amount of money you would like to pay for {duration}hours: $")
           server(duration,cpu,budget)#case2 us-east
           
       elif (cpu > 0):
-            budget = int(input("Enter a budget, enter 0 if you dont have any: "))
+            budget = int(input("Enter a budget, enter 0 if you dont have any: $"))
             if (budget == 0):
               server(duration,cpu,budget)#case1 us-east 
             elif (budget > 0):
@@ -90,16 +118,3 @@ if __name__=="__main__":
       else:
         print("Invalid CPU size")
   
-
-
-
-
-    # cpu = input("Enter number of CPU you require: ")
-    # duration = input("Enter the duration in hours: ")  
-    # server = int(input("Enter 1 for US-EAST and 2 for US-WEST: "))
-    # if( server == 1):
-    #   server()
-    # elif( server == 2):
-    #   usWest()
-    # else:
-    #   print("Enter valid server")
