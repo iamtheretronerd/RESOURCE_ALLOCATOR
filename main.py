b3bbf2b184835c7e610bc48fbdbbf8d4621bbfc1
Hemang Murugan <52290764+iamtheretronerd@users.noreply.github.com>
2021-05-28 17:04:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def server(duration,cpu,budget):
   usWestPrice = [2.97,1.3,0.89,0.413,0.14]
   duration = float(duration)
   budget = float(budget)
-  print(duration,cpu,budget)
+  #print(duration,cpu,budget)
   if(cpu == 0):
     # print("Case 2")
     # CASE 2
@@ -91,30 +91,93 @@ def server(duration,cpu,budget):
         print("region US West")
         print(f"Total Cost ${round(wpricewduration,1)}")
         print(f"Servers:\nlarge: {wnewlist[4]}\n2xlarge: {wnewlist[3]}\n4xlarge: {wnewlist[2]}\n8xlarge: {wnewlist[1]}\n10xlarge: {wnewlist[0]}")
-      
-
 
       
     elif(budget > 0):
-      print("Case 3")
+      # print("Case 3")
+      # CASE 3
+      newlist = [0,0,0,0,0,0]
+      newprice = [0,0,0,0,0,0]
+      balance = cpu
+      finalPrice = 0
+      pricewduration = 0
+      div = int(32)
+      for i in range (6):
+          newvar = int(balance/div) 
+          newlist[i] = newvar
+          if(newlist[i] != 0):
+              balance -= (newlist[i] * div)
+          div = div / 2
+                  
+      for i in range (6):
+          newprice[i] = newlist[i] * usEastPrice[i]
+                  
+      for i in range (6):
+          finalPrice = finalPrice + newprice[i]
+      pricewduration = finalPrice * duration
 
+              #US WEST
+              
+      wnewlist = [0,0,0,0,0]
+      wnewprice = [0,0,0,0,0]
+      wbalance = cpu
+      wfinalPrice = 0
+      wdiv = int(32)
+
+      for i in range (5):
+          if (wdiv == 2):
+              wdiv = 1
+          wnewvar = int(wbalance/wdiv) 
+          wnewlist[i] = wnewvar
+          if(wnewlist[i] != 0):
+              wbalance -= (wnewlist[i] * wdiv)
+          if(wdiv!= 1):
+              wdiv = wdiv / 2
+          #print (wnewlist)
+                  
+      for i in range (5):
+          wnewprice[i] = wnewlist[i] * usWestPrice[i]
+          #price of all servers
+          
+      for i in range (5):
+          wfinalPrice = wfinalPrice + wnewprice[i]
+      wpricewduration = wfinalPrice * duration
+
+
+      if(pricewduration > budget):
+          print(f"\nSorry you will have to pay ${round(pricewduration-budget,1)} more to purchase {cpu} CPU for {duration }hours for a US East server")
+      else:
+          print("region US East")
+          print(f"Total Cost ${round(pricewduration,1)}")
+          print(f"Servers:\nlarge: {newlist[5]}\nxlarge: {newlist[4]}\n2xlarge: {newlist[3]}\n4xlarge: {newlist[2]}\n8xlarge: {newlist[1]}\n10xlarge: {newlist[0]}\n")
+          
+      if(wpricewduration > budget):
+          print(f"\nSorry you will have to pay ${round(wpricewduration-budget,1)} more to purchase {cpu} CPU for {duration} hours for a US West server")
+      else: 
+          print("region US West")
+          print(f"Total Cost ${round(wpricewduration,1)}")
+          print(f"Servers:\nlarge: {wnewlist[4]}\n2xlarge: {wnewlist[3]}\n4xlarge: {wnewlist[2]}\n8xlarge: {wnewlist[1]}\n10xlarge: {wnewlist[0]}")
 
 if __name__=="__main__":
       name = input("Enter your Name: ")
       print("Hello "+name)
-      duration = input("Enter the duration(hours) you want for your servers: ")
-     # region = int(input("Enter 1 for US-EAST and 2 for US-WEST: "))
-      cpu = int(input("Enter number of CPU, Enter 0 if you dont know: "))
-      if (cpu == 0):
-          budget = input(f"Enter the amount of money you would like to pay for {duration}hours: $")
-          server(duration,cpu,budget)#case2 us-east
-          
-      elif (cpu > 0):
-            budget = int(input("Enter a budget, enter 0 if you dont have any: $"))
-            if (budget == 0):
-              server(duration,cpu,budget)#case1 us-east 
-            elif (budget > 0):
-              server(duration,cpu,budget)#case3 us-east
+      duration = int(input("Enter the duration(hours) you want for your servers: "))
+      if(duration > 1):
+          cpu = int(input("Enter number of CPU, Enter 0 if you dont know: "))
+          if (cpu == 0):
+              budget = input(f"Enter the amount of money you would like to pay for {duration}hours: $")
+              server(duration,cpu,budget)#case2 us-east
+              
+          elif (cpu > 0):
+                budget = int(input("Enter a budget, enter 0 if you dont have any: $"))
+                if (budget == 0):
+                  server(duration,cpu,budget)#case1 us-east 
+                elif (budget > 0):
+                  server(duration,cpu,budget)#case3 us-east
+          else:
+            print("Invalid CPU size")
       else:
-        print("Invalid CPU size")
-  
+        print("Invalid Duration")
+
+
+
